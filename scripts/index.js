a4951658ef6f217d4910d5ca94a78eb5b470a34b
Romalus-Creator <86391110+Romalus-Creator@users.noreply.github.com>
2025-02-10 09:58:21 -0600

    Project 4 Stage 3. Add ability to open & close profile edit form on Spots profile page.

--- scripts/index.js
+++ scripts/index.js
@@ -1,43 +1,81 @@
 let initialCards = {
   obj1: {
     name: "Val Thorens",
-    link: URL(
+    link: new URL(
       "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg"
     ),
   },
 
   obj2: {
     name: "Restaurant terrace",
-    link: URL(
+    link: new URL(
       "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg"
     ),
   },
 
   obj3: {
     name: "An outdoor cafe",
-    link: URL(
+    link: new URL(
       "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg"
     ),
   },
 
   obj4: {
     name: "A very long bridge, over the forest and through the trees",
-    link: URL(
+    link: new URL(
       "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg"
     ),
   },
 
   obj5: {
     name: "Tunnel with morning light",
-    link: URL(
+    link: new URL(
       "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg"
     ),
   },
 
   obj6: {
     name: "Mountain house",
-    link: URL(
+    link: new URL(
       "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg"
     ),
   },
 };
+
+//profile__edit-btn .modal__opened
+const editProfileForm = document.querySelector("#edit-modal");
+const editBtn = document.querySelector(".profile__edit-btn");
+const closeBtn = document.querySelector(".modal__close-btn");
+//const submitBtn = document.querySelector(".modal__submit-btn");
+
+// function testClick() {
+//   console.log("clicked button");
+// }
+
+function openModalForm() {
+  editProfileForm.classList.remove("modal__closed");
+  editProfileForm.classList.add("modal__opened");
+}
+
+function closeModalForm() {
+  editProfileForm.classList.remove("modal__opened");
+  editProfileForm.classList.add("modal__closed");
+}
+
+//how to put closeModalForm func inside this func?
+
+editBtn.addEventListener("click", openModalForm);
+
+closeBtn.addEventListener("click", closeModalForm);
+
+// submitBtn.addEventListener("click", function submitModalForm(evt) {
+//   editProfileForm.classList.remove("modal__opened");
+//   editProfileForm.classList.add("modal__closed");
+//   evt.preventDefault();
+//   console.log(evt.target);
+// });
+
+// console.log(editProfileForm);
+// console.log(editBtn);
+
+//need to preventDefault() modal after hitting submitt button -right?
