41ef554f0e5d100f2166c5ff09362e2fb808fbd7
Romalus-Creator <86391110+Romalus-Creator@users.noreply.github.com>
2025-02-12 17:00:00 -0600

    Correct edits and suggestions from Code Reviewer.

--- scripts/index.js
+++ scripts/index.js
@@ -42,39 +42,40 @@ const newProfileName = editModal.querySelector("#profile-name-input");
 const newProfileDesc = editModal.querySelector("#profile-description-input");
 
 const profileFormElement = editModal.querySelector(".modal__form");
-const submitBtn = editModal.querySelector(".modal__submit-btn");
+const submitForm = editModal.querySelector(".modal__form");
 
-function submitModalForm(event) {
-  event.preventDefault();
-  // console.log("clicked button");
-  profileName.textContent = newProfileName.value;
-  profileDesc.textContent = newProfileDesc.value;
-  closeModalForm();
+//It already looks cleaner! Thank you for the suggestion, code reviewer!
+function fillProfileForm() {
+  newProfileName.value = profileName.textContent;
+  newProfileDesc.value = profileDesc.textContent;
 }
 
 function openModalForm() {
-  newProfileName.value = profileName.textContent;
-  newProfileDesc.value = profileDesc.textContent;
-  editModal.classList.remove("modal__closed");
-  editModal.classList.add("modal__opened");
+  editModal.classList.remove("modal--closed");
+  fillProfileForm();
 }
 
 function closeModalForm() {
-  editModal.classList.remove("modal__opened");
-  editModal.classList.add("modal__closed");
+  editModal.classList.add("modal--closed");
+}
+
+function submitModalForm(event) {
+  profileName.textContent = newProfileName.value;
+  profileDesc.textContent = newProfileDesc.value;
+  event.preventDefault();
+  closeModalForm();
 }
 
 editBtn.addEventListener("click", openModalForm);
 
 closeBtn.addEventListener("click", closeModalForm);
 
-submitBtn.addEventListener("click", submitModalForm);
+submitForm.addEventListener("submit", submitModalForm);
 
 const cardTemplate = document.querySelector("#post-template");
 const cardsList = document.querySelector(".posts__list");
 
 function getCardElement(initialCards) {
-  // console.log(initialCards);
   const cardElement = cardTemplate.content
     .querySelector(".post")
     .cloneNode(true);
@@ -83,16 +84,13 @@ function getCardElement(initialCards) {
   // set the image’s src attribute to the image to the link field of the object
   const cardElementImage = cardElement.querySelector(".post__image");
   const cardElementText = cardElement.querySelector(".post__text");
-  const cardElementAlt = cardElement.querySelector(".post__text");
 
   // set the image’s src attribute to the image to the link field of the object
   // set the image’s alt text to the name field of the object
   // set the card’s title to the name field of the object, too
   cardElementImage.src = initialCards.link;
   cardElementText.textContent = initialCards.name;
-  cardElementText.alt = initialCards.name;
-  console.log(cardElementImage);
-  // console.log(cardElementText);
+  cardElementImage.alt = initialCards.name;
 
   // return the ready HTML element with the filled-in data
   return cardElement;
