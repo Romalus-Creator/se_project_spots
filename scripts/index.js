04f920ed9f9a0843ee005725d1b13bdd1198b7da
Romalus-Creator <86391110+Romalus-Creator@users.noreply.github.com>
2025-03-05 19:29:01 -0600

    Fix: Made closeBtn.forEach function per Code Reviewer's suggestion. Fix: remove indexes in const variables, add 'const' in front of two variables. Fix: remove if statment for classList.toggle Fix: Adjust querySelector in a few variables to search inside the specific modal instead of entire document. Fix: Rename openModalForm and closeModalForm by removing 'Form' from function names. Fix: Update two functions by moving a few lines of code into each funciton that are originally outside the functions. Fix: Resize trashcan icons to specifications. Adjust height of posts' images to meet spec. Fix: Increase width of previw image modal's image in medi query for mobile devices.

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-let initialCards = [
+const initialCards = [
   {
     name: "Val Thorens",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
@@ -35,6 +35,12 @@ let initialCards = [
   },
 ];
 
+// ============================= PREVIEW [IMAGE] MODAL =============================
+const previewModal = document.querySelector("#preview-modal");
+const previewModalCloseBtn = previewModal.querySelector(".modal__close-btn");
+const modalImage = previewModal.querySelector(".modal__image");
+const modalCaption = previewModal.querySelector(".modal__caption");
+
 // ============================= LOAD POSTS ONTO PAGE =============================
 const cardTemplate = document.querySelector("#post-template");
 const cardsList = document.querySelector(".posts__list");
@@ -59,14 +65,7 @@ function getCardElement(initialCard) {
   //Function to change CSS on 'heart-shaped' Like Icon for each post.
   const cardLikeBtn = cardElement.querySelector(".post__like-btn");
   cardLikeBtn.addEventListener("click", () => {
-    if (cardLikeBtn.classList[1] == "post__like-btn_clicked") {
-      cardLikeBtn.classList.remove("post__like-btn_clicked");
-      // console.log(cardLikeBtn.classList);
-      // console.log(cardLikeBtn.classList[1]);
-    } else {
-      cardLikeBtn.classList.add("post__like-btn_clicked");
-      // console.log(cardLikeBtn.classList);
-    }
+    cardLikeBtn.classList.toggle("post__like-btn_clicked");
   });
 
   //Make Trash Can icon delete the respective post.
@@ -76,12 +75,8 @@ function getCardElement(initialCard) {
   });
 
   //Click Event anywhere on the post__image will open the preview-modal to better view the picture.
-  const postImage = cardElement.querySelector(".post__image");
-  const previewModal = document.querySelector("#preview-modal");
-  const modalImage = previewModal.querySelector(".modal__image");
-  const modalCaption = previewModal.querySelector(".modal__caption");
-  postImage.addEventListener("click", () => {
-    previewModal.classList.remove("modal_closed");
+  cardElementImage.addEventListener("click", () => {
+    openModal(previewModal);
     modalImage.src = cardElementImage.src;
     modalImage.alt = cardElementImage.alt;
     modalCaption.textContent = cardElementText.textContent;
@@ -93,7 +88,7 @@ function getCardElement(initialCard) {
 }
 
 initialCards.forEach((element) => {
-  cardResult = getCardElement(element);
+  const cardResult = getCardElement(element);
   // Use the appropriate built-in DOM method to add this HTML element to the page.
   cardsList.append(cardResult);
 });
@@ -106,108 +101,90 @@ initialCards.forEach((element) => {
 // }
 
 /* TO DO TASK 2 OF 7:
-- DONE I need to 'invert' my functions. I currently have the fillProfileForm func tucked inside the openModalForm func, but it should be the opposite.
-- DONE Inverting the func's will allow me to use openModalForm in any fill'x'Form func - like the New Post Form that needs a func to open and close still.
-- DONE After checking that this works with the openModalForm func, invert the funcs for the closeModalForm.
+- DONE I need to 'invert' my functions. I currently have the fillProfileForm func tucked inside the openModal func, but it should be the opposite.
+- DONE Inverting the func's will allow me to use openModal in any fill'x'Form func - like the New Post Form that needs a func to open and close still.
+- DONE After checking that this works with the openModal func, invert the funcs for the closeModal.
 - I was able to abstract the open Modal fairly easily, but the close Modal is kicking my butt. I since there are two different div's with .modal class,
 I imagine I need to run down the 'list' of the two div's, search if .modal_closed class is active (or removed?), then return the .modal[i] to pass the close-btn event function to the correct modal.
 */
 
 // ============================= ALL MODALS =============================
 const mainSection = document.querySelector(".main");
-const modal = [...document.querySelectorAll(".modal")];
+const modals = [...document.querySelectorAll(".modal")];
+const closeButtons = document.querySelectorAll(".modal__close-btn");
 
-function openModalForm(modal) {
+function openModal(modal) {
   modal.classList.remove("modal_closed");
 }
 
-function closeModalForm(modal) {
+function closeModal(modal) {
   modal.classList.add("modal_closed");
   // console.log(modal);
 }
 
+//Thank you code reviewer as this blew my mind! I knew I was using the Indexes wrong, but the piece of the 'puzzle' I was missing was the .closest of the modal class.
+closeButtons.forEach((button) => {
+  const popup = button.closest(".modal");
+  button.addEventListener("click", () => closeModal(popup));
+});
+
 // ============================= EDIT PROFILE MODAL =============================
 const editModal = document.querySelector("#edit-modal");
 const editBtn = document.querySelector(".profile__edit-btn");
-const editModalCloseBtn = [
-  ...document.querySelectorAll(".modal__close-btn"),
-][0];
+const editModalCloseBtn = editModal.querySelector(".modal__close-btn");
 const profileName = mainSection.querySelector(".profile__name");
 const profileDesc = mainSection.querySelector(".profile__description");
 const newProfileName = editModal.querySelector("#profile-name-input");
 const newProfileDesc = editModal.querySelector("#profile-description-input");
-const profileFormElement = editModal.querySelector(".modal__form");
-const submitProfileEditForm = editModal.querySelector(".modal__form");
-
-editModalCloseBtn.addEventListener("click", () => {
-  closeModalForm(modal[0]);
-});
+const profileForm = editModal.querySelector(".modal__form");
 
 editBtn.addEventListener("click", () => {
   newProfileName.value = profileName.textContent;
   newProfileDesc.value = profileDesc.textContent;
-  openModalForm(editModal);
+  openModal(editModal);
 });
 
 function submitProfileForm(event) {
-  event.preventDefault();
-  closeModalForm(modal[0]);
-}
-
-submitProfileEditForm.addEventListener("submit", () => {
   profileName.textContent = newProfileName.value;
   profileDesc.textContent = newProfileDesc.value;
-  submitProfileForm(event);
+  event.preventDefault();
+  closeModal(editModal);
+}
+
+profileForm.addEventListener("submit", (profileData) => {
+  submitProfileForm(profileData);
 });
 
 // ============================= NEW POST MODAL =============================
 const newPostModal = document.querySelector("#post-modal");
 const newPostBtn = document.querySelector(".profile__post-btn");
-const newPostModalCloseBtn = [
-  ...document.querySelectorAll(".modal__close-btn"),
-][1];
+const newPostModalCloseBtn = newPostModal.querySelector(".modal__close-btn");
 const submitNewPostForm = newPostModal.querySelector(".modal__form");
 const imageLink = newPostModal.querySelector("#image-link");
 const caption = newPostModal.querySelector("#caption");
 
 newPostBtn.addEventListener("click", () => {
-  openModalForm(newPostModal);
-});
-
-newPostModalCloseBtn.addEventListener("click", () => {
-  closeModalForm(modal[1]);
+  openModal(newPostModal);
 });
 
 function submitNewPost(event) {
   event.preventDefault();
-  closeModalForm(modal[1]);
-}
-
-submitNewPostForm.addEventListener("submit", () => {
+  closeModal(newPostModal);
   const newCard = {
     link: imageLink.value,
     name: caption.value,
   };
-  submitNewPost(event);
 
-  newCardResult = getCardElement(newCard);
+  const newCardResult = getCardElement(newCard);
   cardsList.prepend(newCardResult);
-
   //Remove values to ensure Placeholder text reappears each time the 'New Post' button is clicked.
-  imageLink.value = "";
-  caption.value = "";
+  event.target.reset();
+}
 
+submitNewPostForm.addEventListener("submit", (newPostData) => {
+  submitNewPost(newPostData);
   // Link to an image form unsplash.com that I used to ensure everything works properly:
   // https://plus.unsplash.com/premium_photo-1734543942836-3f9a0c313da4?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8
-});
-
-// ============================= PREVIEW [IMAGE] MODAL =============================
-const previewModalCloseBtn = [
-  ...document.querySelectorAll(".modal__close-btn"),
-][2];
-previewModalCloseBtn.addEventListener("click", () => {
-  // console.log(modal);
-  modal[2].classList.add("modal_closed");
 });
 
 /* TASK 3/7 TO DO:
